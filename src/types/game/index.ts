e58cbbdcdf5e3647b1faeaf8284c2b16ea4d9a92
Clément <clementfiesta@gmail.com>
2024-10-29 11:21:52 +0100

    Fix type definition

--- src/types/game/index.ts
+++ src/types/game/index.ts
@@ -1,5 +1,5 @@
 import { Game } from '@prisma/client';
-import { GameDetails, Game as RawgGame } from '../rawg';
+import { RawgGameDetails as GameDetails, RawgGame } from '../rawg';
 
 export type CombinedGame = Game & {
   rawgData: RawgGame & GameDetails;
