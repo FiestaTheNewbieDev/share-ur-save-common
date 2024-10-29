68153064eba22b302e8c8095e887b84945992df0
Clément <clementfiesta@gmail.com>
2024-10-29 11:00:44 +0100

    Modules split

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import { User as PrismaUser, Game as PrismaGame } from "@prisma/client";
-
 export { PrismaClient } from "@prisma/client";
-export * as types from "./types";
-export namespace prismaTypes {
-    export type User = PrismaUser;
-    export type Game = PrismaGame;
-}
