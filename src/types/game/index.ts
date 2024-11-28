64949f6c6eaeb0171a3d9a4bcb92598eab73c62f
Clément <clementfiesta@gmail.com>
2024-11-28 19:17:54 +0100

    Add GameSearchResult type

--- src/types/game/index.ts
+++ src/types/game/index.ts
@@ -1,6 +1,10 @@
 import { Game } from "../../../prisma/client";
 import { RawgGameDetails as GameDetails, RawgGame } from "../rawg";
 
+export type GameSearchResult = Game & {
+  rawgData: RawgGame;
+};
+
 export type CombinedGame = Game & {
   rawgData: RawgGame & GameDetails;
 };
