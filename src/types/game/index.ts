6e8d5ac26302d4c6118a109bbc438f7e36a3d60e
Clément <clementfiesta@gmail.com>
2024-11-28 19:57:39 +0100

    Simplify GameSearchResult

--- src/types/game/index.ts
+++ src/types/game/index.ts
@@ -2,7 +2,7 @@ import { Game } from "../../../prisma/client";
 import { RawgGameDetails as GameDetails, RawgGame } from "../rawg";
 
 export type GameSearchResult = Game & {
-  rawgData: RawgGame;
+  rawgData: Pick<RawgGame, "id" | "slug" | "name" | "released" | "background_image">;
 };
 
 export type CombinedGame = Game & {
