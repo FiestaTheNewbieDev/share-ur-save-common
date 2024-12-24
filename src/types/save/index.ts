5618af78eb5e7c26707b1741d4fa6ce504e32cff
Clément <clementfiesta@gmail.com>
2024-12-24 13:53:02 +0100

    Add SavesTabs type

--- src/types/save/index.ts
+++ src/types/save/index.ts
@@ -1,6 +1,12 @@
 import { Save, User } from "../../../prisma/client";
 
-
 export type AggregatedSave = Save & {
-    author: Pick<User, 'uuid' | 'username' | 'displayName'>;
-}
+  author: Pick<User, "uuid" | "username" | "displayName">;
+};
+
+export type SavesTabs =
+  | "new-today"
+  | "new-this-week"
+  | "new-this-month"
+  | "latest"
+  | "popular";
