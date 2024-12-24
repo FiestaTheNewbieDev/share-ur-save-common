d4fc18b8a9459c6dc2bee2e38343b1af5e62f7e5
Clément <clementfiesta@gmail.com>
2024-12-24 10:37:56 +0100

    Test to add Prisma request types

--- src/client/PrismaClient.ts
+++ src/client/PrismaClient.ts
@@ -1,11 +1,12 @@
 import { Prisma, PrismaClient as BasePrismaClient } from "../../prisma/client";
 
 type ExtendedMiddlewareParams = Prisma.MiddlewareParams & {
-  args?: {
-    includeDeleted?: boolean;
-    hardDelete?: boolean;
-    [key: string]: any;
-  };
+  args?: ExtendedArgs<any>;
+};
+
+type ExtendedArgs<T> = T & {
+  includeDeleted?: boolean;
+  hardDelete?: boolean;
 };
 
 export default class PrismaClient extends BasePrismaClient {
@@ -18,6 +19,20 @@ export default class PrismaClient extends BasePrismaClient {
     params: ExtendedMiddlewareParams,
     next: (params: ExtendedMiddlewareParams) => Promise<any>
   ) {
+    if (["findUnique", "findFirst", "findMany"].includes(params.action)) {
+      const includeDeleted = params.args?.includeDeleted;
+
+      if (includeDeleted !== undefined) {
+        delete params.args.includeDeleted;
+      }
+
+      if (!includeDeleted) {
+        if (!params.args) params.args = {};
+        if (!params.args.where) params.args.where = {};
+        params.args.where.deletedAt = null;
+      }
+    }
+
     if (params.action === "delete" && params.args?.hardDelete === undefined) {
       params.action = "update";
       params.args.data = { deletedAt: new Date() };
@@ -35,20 +50,26 @@ export default class PrismaClient extends BasePrismaClient {
       }
     }
 
-    if (["findUnique", "findFirst", "findMany"].includes(params.action)) {
-      const includeDeleted = params.args?.includeDeleted;
-
-      if (includeDeleted !== undefined) {
-        delete params.args.includeDeleted;
-      }
-
-      if (!includeDeleted) {
-        if (!params.args) params.args = {};
-        if (!params.args.where) params.args.where = {};
-        params.args.where.deletedAt = null;
-      }
-    }
-
     return next(params);
+  }
+
+  async findUnique<T>(model: keyof PrismaClient, args: ExtendedArgs<F>) {
+    return (this as any)[model].findUnique(args);
+  }
+
+  async findFirst<T>(model: keyof PrismaClient, args: ExtendedArgs<any>) {
+    return (this as any)[model].findFirst(args);
+  }
+
+  async findMany<T>(model: keyof PrismaClient, args: ExtendedArgs<any>) {
+    return (this as any)[model].findMany(args);
+  }
+
+  async delete<T>(model: keyof PrismaClient, args: ExtendedArgs<any>) {
+    return (this as any)[model].delete(args);
+  }
+
+  async deleteMany<T>(model: keyof PrismaClient, args: ExtendedArgs<any>) {
+    return (this as any)[model].deleteMany(args);
   }
 }
