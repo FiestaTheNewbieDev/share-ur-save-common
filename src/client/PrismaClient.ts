2bff0f0381afa73b3b3ee6898b0e9d38925749b2
Clément <clementfiesta@gmail.com>
2024-12-29 14:20:20 +0100

    Add hasSoftDelete verification

--- src/client/PrismaClient.ts
+++ src/client/PrismaClient.ts
@@ -23,57 +23,39 @@ export default class PrismaClient extends BasePrismaClient {
   ) {
     const hasSoftDelete = MODELS_WITH_SOFT_DELETE.includes(params.model || "");
 
-    if (["findUnique", "findFirst", "findMany"].includes(params.action)) {
-      const includeDeleted = params.args?.includeDeleted;
+    if (hasSoftDelete) {
+      if (["findUnique", "findFirst", "findMany"].includes(params.action)) {
+        const includeDeleted = params.args?.includeDeleted;
 
-      if (includeDeleted !== undefined) {
-        delete params.args.includeDeleted;
+        if (includeDeleted !== undefined) {
+          delete params.args.includeDeleted;
+        }
+
+        if (!includeDeleted) {
+          if (!params.args) params.args = {};
+          if (!params.args.where) params.args.where = {};
+          params.args.where.deletedAt = null;
+        }
       }
 
-      if (!includeDeleted && hasSoftDelete) {
-        if (!params.args) params.args = {};
-        if (!params.args.where) params.args.where = {};
-        params.args.where.deletedAt = null;
-      }
-    }
-
-    if (params.action === "delete" && params.args?.hardDelete === undefined) {
-      params.action = "update";
-      params.args.data = { deletedAt: new Date() };
-    }
-
-    if (
-      params.action === "deleteMany" &&
-      params.args?.hardDelete === undefined
-    ) {
-      params.action = "updateMany";
-      if (params.args.data !== undefined) {
-        params.args.data.deletedAt = new Date();
-      } else {
+      if (params.action === "delete" && params.args?.hardDelete === undefined) {
+        params.action = "update";
         params.args.data = { deletedAt: new Date() };
+      }
+
+      if (
+        params.action === "deleteMany" &&
+        params.args?.hardDelete === undefined
+      ) {
+        params.action = "updateMany";
+        if (params.args.data !== undefined) {
+          params.args.data.deletedAt = new Date();
+        } else {
+          params.args.data = { deletedAt: new Date() };
+        }
       }
     }
 
     return next(params);
-  }
-
-  async findUnique<T>(model: keyof PrismaClient, args: ExtendedArgs<any>) {
-    return (this as any)[model].findUnique(args);
-  }
-
-  async findFirst<T>(model: keyof PrismaClient, args: ExtendedArgs<any>) {
-    return (this as any)[model].findFirst(args);
-  }
-
-  async findMany<T>(model: keyof PrismaClient, args: ExtendedArgs<any>) {
-    return (this as any)[model].findMany(args);
-  }
-
-  async delete<T>(model: keyof PrismaClient, args: ExtendedArgs<any>) {
-    return (this as any)[model].delete(args);
-  }
-
-  async deleteMany<T>(model: keyof PrismaClient, args: ExtendedArgs<any>) {
-    return (this as any)[model].deleteMany(args);
   }
 }
